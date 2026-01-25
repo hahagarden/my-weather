1d42a62d8ec4be5765fb2c87c7077e54d8e94c9f
hahagarden <tingyuan413@gmail.com>
2026-01-26 00:55:38 +0900

    fix: 하이드레이션 에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
